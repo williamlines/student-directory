b1eba447730b3b59547dbc11bf5c55795776cc39
William Lines <williamlines42@gmail.com>
2022-10-03 12:21:23 +0100

    refactored student count and fixed typo

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+student_count = 11
+
 #first we print the list of students
 puts "The students of Villains Academy"
 puts "---------------------"
@@ -12,8 +14,5 @@ puts "Freddy Kreuger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
-#finally we print the total number of studetns
-print "Overall we have "
-#its important that print() doesn't add new line characters
-print 11
-puts " great students."
+#finally we print the total number of students
+puts "Overall we have #{student_count} great students"
